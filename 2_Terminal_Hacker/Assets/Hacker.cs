fc5db30836cd3a9ad44ceb4bbb42f93a786bf744
DANIELLE-PC\danielle <thisisimpact@gmail.com>
2018-06-24 14:34:13 -0400

    Refactored OnUserInput method

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -6,9 +6,8 @@ public class Hacker : MonoBehaviour {
 
     // Game state
     int level;
-
     enum Screen { MainMenu, Password, Win };
-    Screen currentScreen = Screen.MainMenu;
+    Screen currentScreen;
 
     // Use this for initialization
     void Start () {
@@ -19,6 +18,7 @@ public class Hacker : MonoBehaviour {
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the library.");
@@ -34,21 +34,30 @@ public class Hacker : MonoBehaviour {
         {
             ShowMainMenu();
         }
-        else if(input == "007")
+        else if(currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+
+    }
+
+    private void RunMainMenu(string input)
+    {
+        if (input == "007")
         {
             Terminal.WriteLine("Please select a target, Mr. Bond.");
         }
-        else if(input == "1")
+        else if (input == "1")
         {
             level = 1;
             StartGame();
         }
-        else if(input == "2")
+        else if (input == "2")
         {
             level = 2;
             StartGame();
         }
-        else if(input == "3")
+        else if (input == "3")
         {
             level = 3;
             StartGame();
