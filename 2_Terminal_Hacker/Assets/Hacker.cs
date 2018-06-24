7ac1d359de82fd512427c3d1992ad62b08fe76cd
DANIELLE-PC\danielle <thisisimpact@gmail.com>
2018-06-24 12:08:14 -0400

    Cleaned up .gitignore

--- 2_Terminal_Hacker/Assets/Hacker.cs
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    // Game state
+    int level;
+
+    // Use this for initialization
+    void Start () {
 
         ShowMainMenu();
 
@@ -13,22 +16,49 @@ public class Hacker : MonoBehaviour {
 
     void ShowMainMenu()
     {
-        var greeting = "Hello User.";
-
         Terminal.ClearScreen();
-        
-        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the library.");
         Terminal.WriteLine("Press 2 for the police station.");
         Terminal.WriteLine("Press 3 for Nasa.");
         Terminal.WriteLine("Enter your selection:");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
+
+    void OnUserInput(string input)
+    {
+        if(input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if(input == "007")
+        {
+            Terminal.WriteLine("Please select a target, Mr. Bond.");
+        }
+        else if(input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if(input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if(input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            Terminal.WriteLine("Please choose a valid target.");
+        }
+    }
+
+    void StartGame()
+    {
+        Terminal.WriteLine("You have chosen target " + level);
+    }
 
 }
